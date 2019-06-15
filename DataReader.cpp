5327e7b5c0abba896cd970e133e36d4bee3115f0
danielenricocahall <danielenricocahall@gmail.com>
2019-06-15 09:37:25 -0400

    initial commit - making datareader class

---
+++ DataReader.cpp
@@ -0,0 +1,27 @@
+/*
+ * DataReader.cpp
+ *
+ *  Created on: Jun 14, 2019
+ *      Author: daniel
+ */
+
+
+
+#include "DataReader.h"
+#include <curl/curl.h>
+#include <cstdint>
+#include <iostream>
+#include <memory>
+#include <boost/asio.hpp>
+#include <stdlib.h>
+
+
+DataReader::DataReader() {
+
+}
+
+DataReader::~DataReader() {
+
+}
+
+
---
+++ DataReader.h
@@ -0,0 +1,20 @@
+/*
+ * Driver.cpp
+ *
+ *  Created on: Jun 14, 2019
+ *      Author: daniel
+ */
+
+#include <string>
+#include <jsoncpp/json/json.h>
+
+class DataReader {
+
+
+public:
+	DataReader();
+	virtual ~DataReader();
+
+private:
+
+};
